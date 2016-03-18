10bb83c3d05c76cd8b4fba805f48933c5e1ca785
elwin_mao <elwin_mao@163.com>
2016-03-18 10:22:57 -0400

    fix

--- bare_metal_autotest/android_auto/android_auto.sh
+++ bare_metal_autotest/android_auto/android_auto.sh
@@ -5,10 +5,10 @@
 ip_linux="192.168.2.16"
 ip_android="192.168.2.34"
 
-# ./my_android_x86_iso_install.sh  $android_ip
-# ./my_android_x86_uefi_install.sh  $android_ip
-# ./my_fastboot.sh $android_ip  reboot 
 
+
+
+###only install one time. ###mybe fix install many times
 ./android_tool.sh ${ip_linux} android_x86_uefi_install.sh
 
 
@@ -19,12 +19,16 @@ ip_android="192.168.2.34"
 
 #######通过web 上传测试用例还有测试材料。
 
+read -p "please input the ip_linux" ip_linux
+echo ${ip_linux}
+
 ./android_tool.sh ${ip_linux} android_x86_iso_install.sh
 ./android_tool.sh ${ip_linux} fastboot.sh reboot
 
 ##test ip  reacheable
-sleep 120
+read -p "please input the ip_android" ip_android
 ####pause press anykey to continue
+echo ${ip_android}
 
 
 ./adb connect ${ip_android}
