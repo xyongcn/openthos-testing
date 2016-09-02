272c7021ff6cb7169b418e5c45d339c39c834d32
mao ying ming <elwin_mao@163.com>
2016-09-02 13:43:58 +0800

    add symbol

---
+++ lkp_result_web/creat_symbol.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+apt-get install -y  apache2 
+apt-get install -y  php5
+apt-get install -y  libapache2-mod-php5
+
+cd /var/www/html
+ln -s /mnt/freenas/result/ .
+ln -s /mnt/freenas/summary/ .
+ln -s /mnt/freenas/compile/ .
